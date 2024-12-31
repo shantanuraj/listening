2ee3aa28a23b1093946520ca5ffd1ce25f4bf4b8
Shantanu Raj <s@sraj.me>
2024-12-31 01:55:58 +0100

    chore(spotify): Extract http calls

--- pkg/spotify/client.go
+++ pkg/spotify/client.go
@@ -21,8 +21,21 @@ var DefaultClient = &Client{
 }
 
 func (c *Client) Get(ctx context.Context, path string) (*http.Response, error) {
+	return c.do(ctx, "GET", path, nil)
+}
+
+func (c *Client) Put(ctx context.Context, path string, body io.Reader) (*http.Response, error) {
+	return c.do(ctx, "PUT", path, body)
+}
+
+func (c *Client) do(
+	ctx context.Context,
+	method string,
+	path string,
+	body io.Reader,
+) (*http.Response, error) {
 	url := host + path
-	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	req, err := http.NewRequestWithContext(ctx, method, url, body)
 	if err != nil {
 		return nil, err
 	}
@@ -36,18 +49,4 @@ func (c *Client) Get(ctx context.Context, path string) (*http.Response, error) {
 
 func (c *Client) SetToken(token *TokenResponse) {
 	c.token = token
-}
-
-func (c *Client) Put(ctx context.Context, path string, body io.Reader) (*http.Response, error) {
-	url := host + path
-	req, err := http.NewRequestWithContext(ctx, "PUT", url, body)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header = http.Header{
-		"Authorization": []string{"Bearer " + c.token.AccessToken},
-	}
-
-	return c.httpClient.Do(req)
 }
